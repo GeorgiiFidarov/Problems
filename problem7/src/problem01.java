c264cd192314dee9cadea9ec3d297daaeb40948a
GeorgiiFidarov <fidarov371@gmail.com>
2022-10-13 21:32:24 +0300

    easy

---
+++ problem7/src/problem01.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * Create ArrayDeque by name queue
+ * in any way known to you and push the
+ * following four numbers 2, 0, 1, 7 .
+ -------------------------------------------------------
+ * The code for displaying the queue is already written.
+ */
+
+public class problem01 {
+    public static void main(String[] args) {
+        int [] queue = {2,0,1,7};
+        System.out.println(Arrays.toString(queue));
+    }
+}
