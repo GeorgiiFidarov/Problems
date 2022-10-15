0059691ed2f341772c1b5cea69da051b5825b529
GeorgiiFidarov <fidarov371@gmail.com>
2022-10-15 13:41:36 +0300

    Another day in factory

---
+++ Problem10/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Реализуйте метод sqrt(), вычисляющий
+ * квадратный корень числа. В отличие от
+ * Math.sqrt(), это метод при передаче отрицательного
+ * параметра должен бросать исключение java.lang.IllegalArgumentException
+ * сообщением "Expected non-negative number, got ?",
+ * где вместо вопросика будет подставлено фактически
+ * переданное в метод число.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        double z = Dath.sqrt(-1);
+    }
+}
+class Dath{
+    public static double sqrt(double x){
+        if (x<0)
+            throw new IllegalArgumentException("Expected non-negative number, got_ "+x);
+        return Dath.sqrt(x);
+    }
+}
