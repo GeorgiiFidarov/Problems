57e14a785e1f49cb08713864dce8e8570f79d858
GeorgiiFidarov <fidarov371@gmail.com>
2022-10-22 23:06:39 +0300

    Commit

--- JavaRushJamm/src/Main.java
+++ JavaRushJamm/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
 
 }
@@ -16,7 +19,7 @@ class Bugatti {
         return body;
     }
 }
-class Programmer{
+class Programmer {
     private int salary = 1000;
     public int getSalary() {
         return salary;
@@ -26,10 +29,73 @@ class Programmer{
             this.salary = salary;
     }
 
-    public static void main(String[] args) {
-        Programmer programmer = new Programmer();
-        programmer.setSalary(10);
-        System.out.println(programmer.salary);
+
+}
+class iphone{
+    private String model;
+    private String color;
+    private int price;
+
+    public iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof iphone)) return false;
+        if (this.model ==null) return false;
+        iphone iphone = (iphone) o;
+        return price == iphone.price &&
+                Objects.equals(model, iphone.model) &&
+                Objects.equals(color, iphone.color);
     }
 }
+class Memory {
+
+    public static void main(String[] args) {
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        executeDefragmentation(memory);
+        System.out.println(Arrays.toString(memory));
+    }
+
+    public static void executeDefragmentation(String[] array) {
+        int length = array.length;//
+        String[]b = new String[length];//
+        int counter = 0;
+        for (int i = 0; i< b.length;i++){
+            if (array[i]!=null) {
+                array[counter++] = array[i];
+            }
+        }
+        Arrays.fill(array, counter, array.length, null);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
