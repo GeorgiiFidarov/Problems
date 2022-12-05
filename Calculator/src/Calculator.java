2bb533e3eda54c87a4f9ac7c508910fdb53075aa
Georgii <fidarov371@gmail.com>
2022-12-05 18:26:41 +0300

    fucking commit

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,6 +1,5 @@
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.PrintStream;
+import java.awt.*;
+import java.io.*;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Arrays;
@@ -8,46 +7,86 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-
+        Desktop desktop = Desktop.getDesktop();
         Scanner scan = new Scanner(System.in);
         String formula = scan.nextLine();
         String[] calculation = formula.split(" ");
-        String pathToFile = "C:\\Users\\Georgii\\Desktop\\Projects\\ReadWriteToFile\\file.txt";
+        String pathToFolder = "C:\\Users\\Georgii\\Desktop\\Projects\\ReadWriteToFile";
+        File file = new File(pathToFolder+"\\output.txt");
+        File logfile = new File(pathToFolder+"\\logfile.txt");
+        try(PrintStream printStream = new PrintStream(file);
+        PrintStream printLog = new PrintStream(logfile)) {
             if (calculation[1].contains("*")) {
-                try(PrintStream printStream = new PrintStream(pathToFile)) {
-                    printStream.println((double)Integer.parseInt(calculation[0]) * Integer.parseInt(calculation[2]));
-                } catch (NumberFormatException | FileNotFoundException e) {
-                    System.out.println(e.getMessage() + " Digits only please");
+                try {
+                    printStream.println((double) Integer
+                            .parseInt(calculation[0]) * Integer.parseInt(calculation[2]));
+                    printStream.close();
+                    desktop.open(file);
+                } catch (NumberFormatException n) {
+                    printLog.print("Please input number!");
+                    printLog.close();
+                } catch (IOException e) {
+                    printLog.println("IOException");
+                    printLog.close();
                 }
             }
             if (calculation[1].contains("/")) {
                 try {
-                    System.out.println((double)Integer.parseInt(calculation[0]) / Integer.parseInt(calculation[2]));
+                    printStream.println(Integer
+                            .parseInt(calculation[0]) / Integer.parseInt(calculation[2]));
+                    printStream.close();
+                    desktop.open(file);
                 } catch (ArithmeticException e) {
-                    System.out.println("Error! Division " + e.getMessage());
+                    printLog.println("Error! Division by zero");
+                    printLog.close();
                 } catch (NumberFormatException n) {
-                    System.out.println("Error! Not number");
+                    printLog.println("Error! Not number");
+                    printLog.close();
+                } catch (IOException e){
+                    printLog.println(e.getMessage());
+                    printLog.close();
                 }
             }
             if (formula.contains("+")) {
                 try {
-                    System.out.println((double) Integer.parseInt(calculation[0]) + Integer.parseInt(calculation[2]));
+                    printStream.println((double) Integer
+                            .parseInt(calculation[0]) + Integer.parseInt(calculation[2]));
+                    printStream.close();
+                    desktop.open(file);
                 } catch (NumberFormatException e) {
-                    System.out.println(e.getMessage() + " Digits only please");
+                    printLog.println("Error! Not number");
+                    printLog.close();
+                } catch (IOException e){
+                    printLog.println(e.getMessage());
+                    printLog.close();
                 }
             } else if (formula.contains("-")) {
                 try {
-                    System.out.println((double)Integer.parseInt(calculation[0]) - Integer.parseInt(calculation[2]));
+                    printStream.println((double) Integer
+                            .parseInt(calculation[0]) - Integer.parseInt(calculation[2]));
+                    printStream.close();
+                    desktop.open(file);
                 } catch (NumberFormatException e) {
-                    System.out.println(e.getMessage() + " Digits only please");
+                    printLog.println("Error! Not number");
+                    printLog.close();
+                } catch (IOException e){
+                    System.out.println(e.getMessage());
+                    printLog.close();
                 }
             }
-        try {
-            if (!calculation[1].equals("*")&&!calculation[1].equals("/")&&!calculation[1].equals("+")&&!calculation[1].equals("-"))
-                throw new Exception("Enter right parameter");
-        } catch (Exception e) {
+            try {
+                if (!calculation[1].equals("*")
+                        && !calculation[1].equals("/")
+                        && !calculation[1].equals("+")
+                        && !calculation[1].equals("-")) {
+                    printLog.println("Parameter please");
+                }
+            } catch (Exception e) {
+                printLog.println("Parameter please");
+                printLog.close();
+            }
+        }catch (FileNotFoundException f){
             System.out.println("Parameter please");
         }
-
     }
 }
